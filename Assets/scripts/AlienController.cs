97bd21b6183e29de1ea6711d8dc31772621267cb
Jeff Kloosterman <kloosterman.jeff@gmail.com>
2014-10-18 14:45:44 -0400

    Create jump/fall animation

--- Assets/scripts/AlienController.cs
+++ Assets/scripts/AlienController.cs
@@ -8,25 +8,52 @@ public class AlienController : MonoBehaviour {
 
     Animator anim;
 
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 700.0f;
+
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position,
+                                           groundRadius,
+                                           whatIsGround);
+        anim.SetBool("Ground", grounded);
+        anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+
         float move = Input.GetAxis("Horizontal");
         rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
         anim.SetFloat("Speed", Mathf.Abs(move));
-        
-        if (move > 0 && !facingRight) {
+
+        if (move > 0 && !facingRight)
+        {
             Flip();
-        } else if (move < 0 && facingRight) {
+        } else if (move < 0 && facingRight)
+        {
             Flip();
         }
     }
 
-    void Flip() {
+    void Update()
+    {
+        if(grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground", false);
+            rigidbody2D.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    void Flip()
+    {
         facingRight = !facingRight;
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
