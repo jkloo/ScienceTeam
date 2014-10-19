40b49dab4fc9d899c667895382545c6d38ce255a
Jeff Kloosterman <kloosterman.jeff@gmail.com>
2014-10-19 12:33:57 -0400

    Create simple level begin/end and death handling

---
+++ Assets/scripts/NextLevelLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevelLoader : MonoBehaviour {
+
+    public string nextLevel;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Application.LoadLevel(nextLevel);
+        }
+    }
+
+}
